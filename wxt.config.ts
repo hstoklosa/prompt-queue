52350398d8a5f72c93edf315f055f2ac2f4bc5c9
imexotic <hubert.stoklosa23@gmail.com>
2025-07-13 10:24:41 +0100

    refactor: modify project structure to use /src directory

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-vue"],
+  srcDir: "src",
   manifest: {
     name: "PromptQueue",
     description: "Queue up prompts on ChatGPT website",
     permissions: ["storage", "activeTab", "scripting"],
-    host_permissions: ["https://chatgpt.com/*"],
+    host_permissions: ["https://chatgpt.com/*", "https://chat.openai.com/*"],
   },
 });
